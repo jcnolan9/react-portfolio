30a0f0a446aa29933291cea727d63ee09a33d233
Mbaku Starcrush <j.c.nolan9@gmail.com>
2022-02-21 21:23:08 -0800

    implementing contact component

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -3,7 +3,18 @@ import React from 'react'
 function contact() {
 
     return (
+        <form>
+            <label for='name'>Name:</label>
+            <input type='text' id='name' required></input>
+            
+            <label for='email'>Email:</label>
+            <input type='email' id='email' required></input>
 
+            <label for='message'>Send Me A Message</label>
+            <textarea id='message' required></textarea>
+
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 
